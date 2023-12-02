b5879ad66c797857c16cd8e8fbb9c34b05177549
Stefan <euler90h@gmail.com>
2023-12-02 15:55:06 -0500

    Added GOSHORTY_* env variables for configuration. These are overridden by the settings in the api_config.yaml file.

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/didip/tollbooth"
@@ -23,6 +24,7 @@ import (
 type APIConfig struct {
 	Conninfo string `yaml:"conninfo"`;
 	RequestsPerMinute float64 `yaml:"requestsPerMinute"`;
+	ListenAddr string `yaml:"listenAddr"`;
 }
 
 var db *sql.DB
@@ -64,10 +66,6 @@ func setupRouter(api_config *APIConfig) *gin.Engine {
 		log.Fatal("api_config is nil")
 	}
 
-	if db == nil {
-		log.Fatal("no db connection")
-	}
-
 	s, err := sqids.New(sqids.Options{
 		MinLength: 15,
 	})
@@ -87,6 +85,13 @@ func setupRouter(api_config *APIConfig) *gin.Engine {
 	})
 
 	r.POST("/", tollbooth_gin.LimitHandler(limiter), func(c *gin.Context) {
+		if db == nil {
+			db, err = sql.Open("postgres", api_config.Conninfo)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+				return
+			}
+		}
 		true_url := c.Request.FormValue("url")
 		if !isUrlOk(true_url) {
 			c.JSON(http.StatusOK, gin.H{"error": "invalid url"})
@@ -103,7 +108,6 @@ func setupRouter(api_config *APIConfig) *gin.Engine {
 			c.JSON(http.StatusOK, gin.H{"error": "failed to shorten url"})
 			return
 		}
-
 		_, err = db.Query(`INSERT INTO urlmap (short_url,true_url) VALUES ($1,$2)`, short_url, true_url)
 		if err != nil {
 			log.Println(err)
@@ -116,6 +120,13 @@ func setupRouter(api_config *APIConfig) *gin.Engine {
 	})
 
 	r.GET("/:id", func(c *gin.Context) {
+		if db == nil {
+			db, err = sql.Open("postgres", api_config.Conninfo)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+				return
+			}
+		}
 		var true_url string
 		shortened_url_id := c.Params.ByName("id")
 		if err := db.QueryRow(`SELECT (true_url) from urlmap where short_url = $1`, shortened_url_id).Scan(&true_url); err != nil {
@@ -129,30 +140,48 @@ func setupRouter(api_config *APIConfig) *gin.Engine {
 	return r
 }
 
-func main() {
-	content, err := os.ReadFile("api_config.yaml")
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func CreateAppConfig() APIConfig {
 	api_config := APIConfig{}
-	err = yaml.Unmarshal([]byte(content), &api_config)
-	if err != nil {
-		log.Fatal(err)
+	if len(os.Getenv("GOSHORTY_CONNINFO")) > 0 {
+		api_config.Conninfo = os.Getenv("GOSHORTY_CONNINFO")
 	}
 
-	db, err = sql.Open("postgres", api_config.Conninfo)
-	if err != nil {
-		log.Fatal(err)
+	api_config.ListenAddr = "0.0.0.0:8080"
+	if len(os.Getenv("GOSHORTY_LISTENADDR")) > 0 {
+		api_config.ListenAddr = os.Getenv("GOSHORTY_LISTENADDR")
 	}
 
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
+	api_config.RequestsPerMinute = 60.0
+	if len(os.Getenv("GOSHORTY_REQUESTSPERMINUTE")) > 0 {
+		x, err := strconv.ParseFloat(os.Getenv("GOSHORTY_REQUESTSPERMINUTE"), 64)
+		if err != nil {
+			log.Println(err)
+		} else {
+			api_config.RequestsPerMinute = x
+		}
 	}
 
+	_, err := os.Stat("api_config.yaml")
+	if err == nil {
+		content, err := os.ReadFile("api_config.yaml")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = yaml.Unmarshal([]byte(content), &api_config)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return api_config
+}
+
+func main() {
+	api_config := CreateAppConfig()
+	log.Println(api_config)
 	r := setupRouter(&api_config)
-	err = r.Run("127.0.0.1:8080") // TODO: change to RunTLS for HTTPS support.
+	err := r.Run(api_config.ListenAddr) // TODO: change to RunTLS for HTTPS support.
 	if err != nil {
 		log.Fatal(err)
 	}
